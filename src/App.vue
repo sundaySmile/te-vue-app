7c8589a25157ea0d09f9a32d0a181e8a938d849d
limeiling <li.meiling01@whaley.cn>
2018-03-31 16:54:26 +0800

    'init'

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container" >
+    <p v-text="msg"></p>
+  </div>   
+</template>
+
+<script lang="ts">
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Hello world!'
+    };
+  }
+};
+</script>
+
+<style scoped>
+  html {
+    overflow-x: hidden;
+  }
+  html, body, div, ul, li, p {
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, SimSun, sans-serif;
+    background-color: #DEECF9;;
+  }
+  a {
+    text-decoration: none;
+    -webkit-tap-heighlight-color: transparent;
+  }
+</style>
+
